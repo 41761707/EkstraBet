da8bced3d00731ef8f06d8eab28ca3475a27b888
41761707 <wojtczakov@gmail.com>
2024-09-25 04:52:43 +0200

    Przygotowanie pod NBA / NHL

---
+++ web_code/pages/NBA.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.set_page_config(page_title = "NBA", page_icon = "🏀", layout="wide")
+
+import base_site_module
+
+def main():
+    #league, season, name
+    st.header("W TRAKCIE IMPLEMENTACJI")
+    #base = base_site_module.Base(46, 11, "NBA")
+
+if __name__ == '__main__':
+    main()
---
+++ web_code/pages/NHL.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.set_page_config(page_title = "NHL", page_icon = "🏒", layout="wide")
+
+import base_site_module
+
+def main():
+    #league, season, name
+    st.header("W TRAKCIE IMPLEMENTACJI")
+    #base = base_site_module.Base(45, 11, "NHL")
+
+if __name__ == '__main__':
+    main()
