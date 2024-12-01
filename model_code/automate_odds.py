ddb1d6f372e6af763ebe47bec58d600e94f1f89d
41761707 <wojtczakov@gmail.com>
2024-12-01 04:06:17 +0100

    [EB-27] Wspolny modul rozruchowy + usuniecie rundy z update

--- model_code/automate_odds.py
+++ model_code/automate_odds.py
@@ -4,14 +4,17 @@ import sys
 import odds_scraper
 import bet_all
 
-def main():
-    league = int(sys.argv[1])
-    season = int(sys.argv[2])
-    games = sys.argv[3]
+def to_automate(league, season, games):
     odds_scraper.odds_to_automate(league, season, games)
     print("Pobieranie kursów ukończone")
     bet_all.bet_to_automate(league, season, 0)
     print("Generowanie zakładów zakończone")
+
+def main():
+    league = int(sys.argv[1])
+    season = int(sys.argv[2])
+    games = sys.argv[3]
+    to_automate(league, season, games)
 
 if __name__ == '__main__':
     main()
