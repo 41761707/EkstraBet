67540f9234ff008c45a1348fd9f69d8f7e364261
unknown <wojtczakov@gmail.com>
2025-08-09 03:06:09 +0200

    Dodanie nowych lig + rozszerzenie gitignore

--- scrapper_code/scrapper_wrapper.py
+++ scrapper_code/scrapper_wrapper.py
@@ -85,35 +85,35 @@ def main() -> None:
         # '4 12 https://www.flashscore.pl/pilka-nozna/niemcy/bundesliga-2025-2026/', #start: 22.08.2025
         # '5 12 https://www.flashscore.pl/pilka-nozna/wlochy/serie-a-2025-2026/', #start: 23.08.2025
         # '6 12 https://www.flashscore.pl/pilka-nozna/hiszpania/laliga-2025-2026/', #start: 17.08.2025
-        # '7 12 https://www.flashscore.pl/pilka-nozna/portugalia/liga-portugal-2025-2026/', #start: 10.08.2025
-        # '8 12 https://www.flashscore.pl/pilka-nozna/anglia/championship-2025-2026/', #start: 08.08.2025
+        '7 12 https://www.flashscore.pl/pilka-nozna/portugalia/liga-portugal-2025-2026/',
+        '8 12 https://www.flashscore.pl/pilka-nozna/anglia/championship-2025-2026/',
         # '10 11 https://www.flashscore.pl/pilka-nozna/australia/a-league-2024-2025/', #koniec ligi
         '11 12 https://www.flashscore.pl/pilka-nozna/belgia/jupiler-league-2025-2026/',
         '12 12 https://www.flashscore.pl/pilka-nozna/czechy/chance-liga-2025-2026/',
-        # '13 12 https://www.flashscore.pl/pilka-nozna/francja/ligue-2-2025-2026/', #start: 09.08.2025
+        '13 12 https://www.flashscore.pl/pilka-nozna/francja/ligue-2-2025-2026/',
         # '14 12 https://www.flashscore.pl/pilka-nozna/hiszpania/laliga2-2025-2026/', #start: 15.08.2025
         '15 11 https://www.flashscore.pl/pilka-nozna/korea-poludniowa/k-league-1-2025/',
-        # '16 12 https://www.flashscore.pl/pilka-nozna/holandia/eredivisie-2025-2026/', #start: 08.08.2025
+        '16 12 https://www.flashscore.pl/pilka-nozna/holandia/eredivisie-2025-2026/',
         '17 11 https://www.flashscore.pl/pilka-nozna/japonia/j1-league-2025/',
         '19 12 https://www.flashscore.pl/pilka-nozna/meksyk/liga-mx-2025-2026/',
         '20 12 https://www.flashscore.pl/pilka-nozna/niemcy/2-bundesliga-2025-2026/',
         '21 12 https://www.flashscore.pl/pilka-nozna/polska/betclic-1-liga-2025-2026/',
         '23 12 https://www.flashscore.pl/pilka-nozna/szwajcaria/super-league-2025-2026/',
-        # '24 12 https://www.flashscore.pl/pilka-nozna/turcja/super-lig-2025-2026/', #start: 08.08.2025
+        '24 12 https://www.flashscore.pl/pilka-nozna/turcja/super-lig-2025-2026/',
         '25 11 https://www.flashscore.pl/pilka-nozna/usa/mls-2025/',
-        # '26 11 https://www.flashscore.pl/pilka-nozna/wlochy/serie-b-2024-2025/', #koniec ligi
+        # '26 12 https://www.flashscore.pl/pilka-nozna/wlochy/serie-b-2025-2026/', #start: 22.08.2025
         '29 12 https://www.flashscore.pl/pilka-nozna/austria/bundesliga-2025-2026/',
         '30 11 https://www.flashscore.pl/pilka-nozna/korea-poludniowa/k-league-2-2025/',
-        # '31 12 https://www.flashscore.pl/pilka-nozna/holandia/eerste-divisie-2025-2026/', #start: 08.08.2025
+        '31 12 https://www.flashscore.pl/pilka-nozna/holandia/eerste-divisie-2025-2026/',
         '32 11 https://www.flashscore.pl/pilka-nozna/japonia/j2-league-2025/',
         '33 11 https://www.flashscore.pl/pilka-nozna/argentyna/torneo-betano-2025/',
         '34 11 https://www.flashscore.pl/pilka-nozna/brazylia/serie-a-betano-2025/',
         '35 11 https://www.flashscore.pl/pilka-nozna/brazylia/serie-b-2025/',
-        # '36 12 https://www.flashscore.pl/pilka-nozna/portugalia/liga-portugal-2-2025-2026/', #start: 10.08.2025
-        # '37 12 https://www.flashscore.pl/pilka-nozna/belgia/challenger-pro-league-2025-2026/', #start: 08.08.2025
+        '36 12 https://www.flashscore.pl/pilka-nozna/portugalia/liga-portugal-2-2025-2026/',
+        '37 12 https://www.flashscore.pl/pilka-nozna/belgia/challenger-pro-league-2025-2026/',
         '38 12 https://www.flashscore.pl/pilka-nozna/austria/2-liga-2025-2026/',
         '39 12 https://www.flashscore.pl/pilka-nozna/szwajcaria/challenge-league-2025-2026/',
-        # '40 12 https://www.flashscore.pl/pilka-nozna/turcja/1-lig-2025-2026/', #start: 10.08.2025
+        '40 12 https://www.flashscore.pl/pilka-nozna/turcja/1-lig-2025-2026/',
         '41 12 https://www.flashscore.pl/pilka-nozna/czechy/dywizja-2-2025-2026/'
     ]
     parser = argparse.ArgumentParser(
